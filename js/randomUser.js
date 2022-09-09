bee2c5619296bb1bf1ed5011f8beec3dd1d25d4c
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2022-09-09 12:42:22 +0600

    fetch important

---
+++ js/randomUser.js
@@ -0,0 +1,29 @@
+
+
+
+const loadUsers = () => {
+    fetch("https://randomuser.me/api/?results=10")
+        .then(res => res.json())
+        .then(data => displayUsers(data.results));
+}
+const displayUsers = users => {
+    const userContainer = document.getElementById("users-container");
+    for (const user of users) {
+        const userDiv = document.createElement("div");
+        console.log(user)
+        userDiv.classList.add('user')
+        userDiv.innerHTML = `
+        <h3>User Name:${user.name.last}</h3>
+        <p>Email: ${user.email}</p>
+        <p>User Info: </p>
+        <p>Country: ${user.location.country}</p>
+        
+        
+        `;
+        userContainer.appendChild(userDiv);
+    }
+}
+loadUsers();
+displayUsers();
+
+
